79fb49a455963e00db6b79fd79e3986d6dc70ec1
qeftser <qeftser1@gmail.com>
2024-09-23 19:32:21 -0600

    cool demo

--- ncpp/src/scratch.cpp
+++ ncpp/src/scratch.cpp
@@ -3,17 +3,22 @@
 
 int main(void) {
 
-   Grid g = Grid(2.434,-10,10,-10,10);
+   Grid g = Grid(0.075,0,10,0,10);
 
-   Entity e0 = Entity(5,5,2,2,KUROME_TYPE_ELPS,40);
+   Entity e0 = Entity(5,5,2,4,0,KUROME_TYPE_ELPS,40);
+   /*
    Entity e1 = Entity(0,0,2,2,KUROME_TYPE_ELPS,40);
    Entity e2 = Entity(-5,-5,2,2,KUROME_TYPE_ELPS,40);
    Entity e3 = Entity(-10,-10,2,2,KUROME_TYPE_ELPS,40);
+   */
    g.addEntity(&e0);
+   /*
    g.addEntity(&e1);
    g.addEntity(&e2);
    g.addEntity(&e3);
+   */
 
+   /*
    g.info();
    g.print();
 
@@ -52,8 +57,13 @@ int main(void) {
    g.print();
 
    g.changeUnitSize(5);
-   g.info();
-   g.print();
+   */
+   for ( ; ; ) {
+      e0.rot += 1;
+      g.changeUnitSize(0.075);
+      g.print();
+      usleep(10000);
+   }
 
    return 0;
 }
